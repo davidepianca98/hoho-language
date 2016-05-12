0f20062caa37de5da1777febec95a264d6130873
davidepianca98 <davidepianca98@gmail.com>
2016-05-12 13:35:43 +0000

    Added variables

--- parser.c
+++ parser.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+#include "list.h"
+#include "variables.h"
 
 void print_help() {
     printf("Usage: parser <filename>\n");
 }
 
+void savevar(char *line) {
+    var *variable = (var *) malloc(sizeof(var));
+    char *name = strchr(line, ' ') + 1;
+    // remove the \n at the end from fgets
+    name[strcspn(name, "\n")] = 0;
+    strcpy(variable->name, name);
+    list_add(variable);
+}
+
+void variable_op(var *variable, char *line) {
+    // check if it's an assignment
+    if(strncmp(strchr(line, '='), "=", 1) == 0) {
+        variable->integer = atoi(strchr(line, '=') + 2);
+    }
+}
+
 void print(char *line) {
     char *word = strchr(line, ' ') + 1;
-    printf("%s", word);
+    var *vari = list_search(word);
+    if(vari) {
+        // variable with that name exists, just print the value
+        printf("%d", vari->integer);
+    } else {
+        printf("%s", word);
+    }
 }
 
 void command(char *line) {
@@ -17,6 +43,14 @@ void command(char *line) {
     
     if(strcmp(command, "print") == 0) {
         print(line2);
+    } else if(strcmp(command, "var") == 0) {
+        savevar(line2);
+    } else {
+        // no command found, maybe it's a variable operation
+        var *vari = list_search(command);
+        if(vari) {
+            variable_op(vari, line2);
+        }
     }
 }
 
---
+++ variables.h
@@ -0,0 +1,16 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+// max string length
+#define MAX_STRING 256
+
+typedef struct {
+    int type; // indicates which type the variable is
+    char name[MAX_STRING];
+    int integer;
+    float floating;
+    char character;
+    char string[MAX_STRING];
+} var;
+
+#endif
